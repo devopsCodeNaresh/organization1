7c4401adaf7c945c1026b288c6b49aebdacb1141
Naresh Sharma <Naresh_Sharma@epam.com>
2021-05-09 18:44:52 +0530

    testchnage2

---
+++ demo1.js
@@ -0,0 +1,14 @@
+function display(){
+
+    console.log("hello world")
+}
+
+var arr=['rahul','naresh','choduuuuuu']
+
+for(var i=0; i<arr.length; i++){
+
+    if(arr[i]=='rahul'){
+
+        console.log("beer pliani padehi")
+    }
+}
